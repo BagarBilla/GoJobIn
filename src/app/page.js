72b1dfdf9bf466a1b3dab7417adb92fad43e315d
SAM <noobgaming02468@gmail.com>
2026-09-03 17:15:42 +0530

    CareerResources Update

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function Home() {
       <Companies />
       <JobRoles />
       <ResumeTemplate />
-      <Reviews />
-      <CareerResources /> */}
+      <Reviews /> */}
+      <CareerResources />
     </>
   );
 }
